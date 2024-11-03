22790026d83c44d860ae8dd1e3ca5ecb2040a9fe
asoma79 <motasemabusabha1998@gmail.com>
2024-11-03 21:00:24 +0200

    Update configBot.js

--- config/configBot.js
+++ config/configBot.js
@@ -5,7 +5,7 @@ const { TOKEN } = process.env
 
 module.exports = () => {
     
-    const bot = new Telegraf(TOKEN)
+    const bot = new Telegraf(7865067259:AAGU_oTsaufj77YbFgsBrs4Xz761HdwusX0)
 
     consign()
         .include('controllers')
